edbd56745d980ba7b40a41121bce580ff9199c33
InsanAra029 <99909768+InsanAra029@users.noreply.github.com>
2022-02-23 12:30:47 +0600

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,14 @@
+# exercise-1
+# num1,num2,num3=input("Enter number: ").split(",")
+# avg=(int(num1)+int(num2)+int(num3))/3
+# print("The numbers are {}, {}, {} and average is {}".format(num1,num2,num3,avg))
+# print(f"The numbers are {num1}, {num2}, {num3} and average is {avg}")
+# exercise-2
+# name=input("Enter name:")
+# print(f"The reverse name is {name[-1::-1]}")
+# print("the reverse is: "+name[2::-1])
+# exercise-3
+name,char=input("Enter name and char: ").split(",")
+print("name length: " +len(name))
+print("count: "+name.count(char))#case sensitive
+print(f"count: {}")
